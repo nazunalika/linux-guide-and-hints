8fa4b7510871d8da0b5a6e215166e68162a5dfc8
Tommy Nguyen <remyabel@gmail.com>
2018-05-09 10:28:11 -0400

    bump short version

--- source/conf.py
+++ source/conf.py
@@ -56,7 +56,7 @@ author = u'remyabel, nazunalika'
 # built documents.
 #
 # The short X.Y version.
-version = '1.0'
+version = '1.1'
 # The full version, including alpha/beta/rc tags.
 release = '1.1.0'
 
