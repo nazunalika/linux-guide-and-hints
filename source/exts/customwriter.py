d101a9195be48c6938a75781e95d1262a9641061
Tommy Nguyen <remyabel@gmail.com>
2018-09-03 15:57:35 -0400

    fix inconsistent tab/indentation and use newer python

--- source/exts/customwriter.py
+++ source/exts/customwriter.py
@@ -11,53 +11,53 @@ def setup(app):
 
 
 class HTMLWriter(Writer):
-	def __init__(self, builder):
-		Writer.__init__(self)
-		self.builder = builder
+        def __init__(self, builder):
+                Writer.__init__(self)
+                self.builder = builder
 
 class HTMLTranslator(BaseTranslator):
-	def __init__(self, builder, *args, **kwds):
-		BaseTranslator.__init__(self, builder, *args, **kwds)
+        def __init__(self, builder, *args, **kwds):
+                BaseTranslator.__init__(self, builder, *args, **kwds)
 
-	def visit_admonition(self, node, name=''):
-		type = 'primary'
-		if name == 'note':
-			type = 'info'
-		elif name == 'warning':
-			type = 'warning'
-		elif name == 'danger':
-			type = 'danger'
-		self.body.append(self.starttag(
-			node, 'div', CLASS=('panel panel-' + type)))
-		self.body.append(self.starttag(
-			node, 'div', CLASS=('panel-heading')))
+        def visit_admonition(self, node, name=''):
+                type = 'primary'
+                if name == 'note':
+                        type = 'info'
+                elif name == 'warning':
+                        type = 'warning'
+                elif name == 'danger':
+                        type = 'danger'
+                self.body.append(self.starttag(
+                        node, 'div', CLASS=('panel panel-' + type)))
+                self.body.append(self.starttag(
+                        node, 'div', CLASS=('panel-heading')))
 
-	 	title = node.traverse(nodes.paragraph);
+                title = node.traverse(nodes.paragraph);
                 self.body.append(self.starttag(
                         node, 'h3', CLASS=('panel-title')))
                 self.body.append("<i class='fa fa-exclamation-circle'></i> " + name.title());
                 self.body.append('</h3>')
 
-		# node.remove(node.traverse(nodes.paragraph)[0])
+                # node.remove(node.traverse(nodes.paragraph)[0])
 
-		self.body.append("</div>")
-		self.body.append(self.starttag(
-			node, 'div', CLASS=('panel-body')))
+                self.body.append("</div>")
+                self.body.append(self.starttag(
+                        node, 'div', CLASS=('panel-body')))
 
-	def depart_admonition(self, node=None):
-		self.body.append('</div></div>\n')
+        def depart_admonition(self, node=None):
+                self.body.append('</div></div>\n')
 
-	def visit_literal_block(self, node):
-		if node.rawsource != node.astext():
-				# most probably a parsed-literal block -- don't highlight
-				return BaseTranslator.visit_literal_block(self, node)
-		lang = self.highlightlang
-		if 'language' in node:
-			lang = node['language']
-		highlighted = node[0]
+        def visit_literal_block(self, node):
+                if node.rawsource != node.astext():
+                                # most probably a parsed-literal block -- don't highlight
+                                return BaseTranslator.visit_literal_block(self, node)
+                lang = self.highlightlang
+                if 'language' in node:
+                        lang = node['language']
+                highlighted = node[0]
                 
                 html = '<pre><code class="language-%s">%s</code></pre>' % (lang, highlighted)
 
                 self.body.append(html)
 
-		raise nodes.SkipNode
+                raise nodes.SkipNode
