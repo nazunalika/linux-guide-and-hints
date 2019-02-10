64a10ee17214d6a3c80f8e39428edb786b9a7a4e
Tommy Nguyen <remyabel@gmail.com>
2019-02-09 22:04:32 -0500

    Update copyright year

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Linux Guide and Hints'
-copyright = u'2018, remyabel, nazunalika'
+copyright = u'2019, remyabel, nazunalika'
 author = u'remyabel, nazunalika'
 
 # The version info for the project you're documenting, acts as replacement for
