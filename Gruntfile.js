88611f253f87e78d59d43cd5cb11f31586049a88
Tommy Nguyen <remyabel@gmail.com>
2019-03-03 01:25:38 -0500

    Remove copy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,10 +39,9 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-    grunt.registerTask('build', ['copy', 'uglify', 'imagemin']);
+    grunt.registerTask('build', ['uglify', 'imagemin']);
     grunt.registerTask('default', ['build', 'connect']);
 };
