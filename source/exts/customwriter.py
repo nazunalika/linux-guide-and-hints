a42c1c5a6435e8cef97020ab00850fc45f78c5f0
Tommy Nguyen <remyabel@gmail.com>
2021-06-09 09:58:43 -0400

    Remove Bootstrap which we don't need.

--- source/exts/customwriter.py
+++ source/exts/customwriter.py
@@ -19,35 +19,6 @@ class HTMLTranslator(BaseTranslator):
     def __init__(self, builder, *args, **kwds):
         BaseTranslator.__init__(self, builder, *args, **kwds)
 
-    def visit_admonition(self, node, name=''):
-        type = 'primary'
-        if name == 'note':
-            type = 'info'
-        elif name == 'warning':
-            type = 'warning'
-        elif name == 'danger':
-            type = 'danger'
-        self.body.append(self.starttag(
-            node, 'div', CLASS=('card')))
-        self.body.append(self.starttag(
-            node, 'div', CLASS=('card-header bg-' + type)))
-
-        title = node.traverse(nodes.paragraph)[0];
-        self.body.append("<i class='fa fa-exclamation-circle'></i> " + name.title());
-
-        self.body.append("</div>")
-        self.body.append(self.starttag(
-            node, 'div', CLASS=('card-body')))
-        if (len(node.traverse(nodes.paragraph)) > 1):
-            node.remove(node.traverse(nodes.paragraph)[0])
-            self.body.append(self.starttag(
-                node, 'h5', CLASS=('card-title')))
-            self.body.append(title.astext());
-            self.body.append('</h5>')
-
-    def depart_admonition(self, node=None):
-        self.body.append('</div></div>\n')
-
     def visit_literal_block(self, node):
         if node.rawsource != node.astext():
             # most probably a parsed-literal block -- don't highlight
